cb1a8fe29d9a77f883cd58c03d567addf585b0ac
Gene Higgins <hi@genehiggins.com>
2024-11-20 17:24:40 -0700

    first commit

---
+++ wedding_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mvrcxc6=%@$-=$u09@ufjwdv8xh+rjyy645&-92f8np6sum8!g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
